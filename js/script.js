6ab8857524d34ffb80d1bb97ee46c4037c8a8028
Holmfred <fredrik1304@live.no>
2021-12-02 19:43:44 +0100

    More Done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-import {books} from "./constants/data.js"
+//import {books} from "./constants/data.js"
+
+let books = [
+    {
+        isbn: "1600506460320",
+        title: "Great book",
+    },
+    {
+        isbn: "1600506460373",
+        title: "Ok book",
+    },
+    {
+        isbn: "1600506460521",
+        title: "Bad book",
+    },
+    {
+        isbn: "1600506460456",
+        title: "Terrible book",
+    },
+];
 
 function createHTML() {
     const bookContainer = document.querySelector(".book-container")
